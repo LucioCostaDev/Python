4e563dea84c8e05cd3bc72d0f56571eaa8068a21
Lucio Costa <luciosdc@outlook.com>
2024-05-02 06:12:22 -0300

    Update teste.py
    estudando

--- teste.py
+++ teste.py
@@ -13,7 +13,77 @@
 # num4 = input ("digite um número: ")
 # print("=> ", num1 , "=> ", num2, "=> ", num3, "=> ", num4)
 
-a = int(input("Digite um número: "))
-print (a + 5)
+# a = int(input("Digite um número: "))
+# print (a + 5)
 
-int(input("Texto"))
+# int(input("Texto"))
+
+# #Colocando varivel parta realizar calculo
+# print (2*3) #multiplicação
+# print (2**3) #potenciação
+# print (2-3) #subtração
+# print (2+3) #soma
+# print (2/3) #divisão 
+
+# #colocando uma variavel declarada
+# a = 2
+# b = 3
+# print(a+b)
+
+# from cmath import sqrt
+# print(sqrt(4))
+
+# nome = input("Digite seu nome: ")
+# nascimento = input("Qual seu ano de nascimento: ")
+# email = input("Qual seu email: ")
+# print("Nome:",nome,".E-mail:",email,".Nascimento:",nascimento,".")
+
+# idade = int(input("Digite sua idade: "))
+# if idade > 18:
+#     print("É maior de idade")
+# elif idade == 18:
+#     print("É guerreiro Jedi")
+# else:
+#     print("É menor de idade")
+
+# TERNARIOS
+
+# # VALOR FIXO 
+# idade = 18
+# print("É maior de idade") if idade >= 18 else print("É menor")
+
+# # ATRIBUINDO VALOR
+# idade = int(input("Digite uma idade:"))
+# print("É maior de idade") if idade >= 18 else print("É menor")
+
+# SWITCH CASE  NO PYTHON E (CASE MATCH)
+
+# a = "Marcio"
+# match a:
+#     case "Antonio":
+#         print("Não é Marcio")
+#     case "Pedro":
+#         print("É Pedro e não Marcio")
+#     case "Marcio":
+#         # print("Achou o Marcio")
+#         sobrenome = input("Digite seu sobrenome")
+
+# Um algoritmo que:
+# peça ao usuário o para digitar o seu ano de nascimento
+# peça o ano atual
+# descubra se o usuário e maior de idade
+# se for, pede o titulo de eleitor dele
+# senão, pede um documento do responsavel.
+
+print("#####################################################")
+nasc = int(input("Digite seu ano de nascimento: "))
+anoatual = int(input("Digite o ano atual: "))
+idade = (anoatual-nasc)
+if idade >= 16:
+    print("Porfavor apresente o título")
+else:
+    print("Porfavor apresente um documento do responsavel")
+print("#####################################################")
+
+
+

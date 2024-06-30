e38d4956bccec03fb5a42dad93467e695c7377ac
Lucio Costa <luciosdc@outlook.com>
2024-06-30 00:40:36 -0300

    estundo python

---
+++ python 2.0/livropython.py
@@ -0,0 +1,14 @@
+# # Programa calculo de aumento de salário
+# salario = 1500
+# aumento = 5
+# print(salario + (salario * aumento / 100))
+
+# # Faça um programa que exiba seu nome na tela
+# nome = "Lucio Costa"
+# print(nome)
+
+# Escreva um programa que exiba o resultado de 2a x 3b, em que a vale 3 e b vale 5.
+
+# Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela.
+
+# Modifique o Programa 2.2, de forma que ele calcule um aumento de 15% para um salário de R$ 750.

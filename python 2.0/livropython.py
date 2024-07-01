b393a43ab0bfa03ee21103e8b6ac073d453bc4ca
Lucio Costa <luciosdc@outlook.com>
2024-07-01 19:31:36 -0300

    estudando python

--- python 2.0/livropython.py
+++ python 2.0/livropython.py
@@ -3,12 +3,23 @@
 # aumento = 5
 # print(salario + (salario * aumento / 100))
 
-# # Faça um programa que exiba seu nome na tela
+# # 2.3 Faça um programa que exiba seu nome na tela
 # nome = "Lucio Costa"
 # print(nome)
 
-# Escreva um programa que exiba o resultado de 2a x 3b, em que a vale 3 e b vale 5.
+# # 2.4 Escreva um programa que exiba o resultado de 2a x 3b, em que a vale 3 e b vale 5.
+# a = 3
+# b = 5
+# print(2 * a * 3 * b )
 
-# Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela.
+# # 2.5 Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela.
+# a = 1
+# b = 2
+# c = 3
+# print(a + b + c)
 
-# Modifique o Programa 2.2, de forma que ele calcule um aumento de 15% para um salário de R$ 750.
+# # 2.6 Modifique o Programa 2.2, de forma que ele calcule um aumento de 15% para um salário de R$ 750.
+# salario = 750
+# aumento = 15
+# print(salario + (salario * aumento / 100))
+

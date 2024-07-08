b8653d9497ed97f3c26703bf701463d594a9f239
Lucio Costa <luciosdc@outlook.com>
2024-07-08 18:55:42 -0300

    manopipulando arquivo

--- python 2.0/files.py
+++ python 2.0/files.py
@@ -1,8 +1,17 @@
 # Manipulação de dados
-open("Teste.txt", "X")
+# open("Teste.txt", "X")
 # R = Read
 # W = Write
 # X = Execute
 # A = Append
 # R+
-# WB
+# WB5
+
+# arquivo = open("números.txt", "w" )
+# for linha in range (1, 100):
+#     arquivo.write(f"{linha}\n")
+# arquivo.close()
+
+
+arquivo = open("Teste.txt", "x")
+

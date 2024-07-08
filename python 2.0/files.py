6f0dd245f8aa257bec1897de4402c84e6cb7b5ad
Lucio Costa <luciosdc@outlook.com>
2024-07-08 19:27:26 -0300

    manipulando arquivos

--- python 2.0/files.py
+++ python 2.0/files.py
@@ -7,11 +7,23 @@
 # R+
 # WB5
 
+# arquivo = open("Teste.txt", "x")
+
 # arquivo = open("números.txt", "w" )
 # for linha in range (1, 100):
 #     arquivo.write(f"{linha}\n")
 # arquivo.close()
 
+# conteudo = open("arquivo.txt", "a")
+# conteudo.write("Uma linha qualquer\n")
+# conteudo.write("Uma segunda linha qualquer\n")
+# conteudo.write("Uma terceira linha qualquer\n")
+# print(conteudo.read())
 
-arquivo = open("Teste.txt", "x")
-
+#  ler arquivos da internet
+import requests
+ler = requests.get("https://pt.wikipedia.org/wiki/Revolta_Paulista_de_1924")
+print(ler)
+# 
+# r = requests.get("")
+# print(r) 

18cb2e6aec6ec99d02f9b0c14f530d4b6a5cf627
Lucio Costa <luciosdc@outlook.com>
2024-09-04 20:04:34 -0300

    Update testes.py

--- python 2.0/testes.py
+++ python 2.0/testes.py
@@ -1,6 +1,12 @@
 # x = input ("Digite um número: ")
 # print(x)
 
-nome = input("Digite seu nome:")
-print(f"Você digitou {nome}")
-print(f"Olá, {nome}!")
+# nome = input("Digite seu nome:")
+# print(f"Você digitou {nome}")
+# print(f"Olá, {nome}!")
+
+#### EXERCICIOS #######
+int1 =  int(input("Digite um número inteiro: "))
+int2 =  int(input("Digite um  segundo número inteiro: "))
+soma = int1 + int2
+print("A soma dos dois números inteiros é:", soma)

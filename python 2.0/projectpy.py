1a680701b282e64729077718d3e3e8dc7755286e
Lucio Costa <luciosdc@outlook.com>
2024-09-26 06:15:18 -0300

    estudando py

---
+++ python 2.0/projectpy.py
@@ -0,0 +1,14 @@
+# PythonFlix
+# nome = "Matrix"
+# ano_lancamento = 2024
+# nota_filme = 10.0
+# plano = False" 
+
+# print(type(nome))
+# print(type(ano_lancamento))
+# print(type(nota_filme))
+# print(type(plano))
+
+filme = input("Digite um nome de filme: \n")
+
+print(filme)

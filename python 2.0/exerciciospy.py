7e197e2605e98e841b7feb8c966ebfd7c8511e5c
Lucio Costa <luciosdc@outlook.com>
2024-11-17 18:42:59 -0300

    treinando py

---
+++ python 2.0/exerciciospy.py
@@ -0,0 +1,14 @@
+print("Programa Aprendizado")
+opcao = input("Quer entrar? Sim (s) ou Não(n)")
+if opcao == "s":
+    print("Vamos começar")
+else:
+    print("Até um proxima vez.")
+print("Parabens vamos iniciar seu aprendizado")
+opcao1 = input("Vamos fazer um Quiz? SIM (s) ou Não(n)")
+if opcao1 == "s":
+    print("Quais Materias tema vc quer?")
+else:
+    print("Volte numa  opção.")
+
+    

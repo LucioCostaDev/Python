343cdfb37cfcd22d827cc8463bb4d607e6f998e3
Lucio Costa <luciosdc@outlook.com>
2024-12-24 14:42:32 -0300

    Create exercicios.py

---
+++ python 2.0/exercicios/exercicios.py
@@ -0,0 +1,11 @@
+
+print("Comparação de Números")
+print("Digite dois números")
+numero1 = input(int("Digite um numero 1: "))
+numero2 = input(int("Digite um numero 2: "))
+
+if numero1 > numero2
+    print(f"{numero1} e maior que numero {numero2}")
+    elif numero1 < numero2
+    print(f"{numero1} e menor que numero {numero2}")
+    
